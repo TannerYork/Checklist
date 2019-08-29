16b5fa3243fbabe691743b14a5e1d6e0c6d846f5
Tanner York <tannerwyork@gmail.com>
2019-08-28 19:17:44 -0700

    Add checklist and corisponding CRUD operations

--- checklist.py
+++ checklist.py
@@ -1,1 +1,34 @@
-print('Hello World')
+checklist = list()
+
+# CREATE
+def create(item):
+    checklist.append(item)
+
+# READ
+def read(index):
+    return checklist[index]
+
+# UPDATE
+def update(index, item):
+    checklist[index] = item
+
+# Destroy
+def destroy(index):
+    checklist.pop(index)
+
+def test ():
+    create('purple sox')
+    create('red cloaks')
+
+    print(read(0))
+    print(read(1))
+
+    update(0, 'purple socks')
+    destroy(1)
+
+    print(read(0))
+    print(read(1))
+
+test()
+
+
