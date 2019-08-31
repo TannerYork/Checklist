2cb92b024c9b4905a287ded358d55aa979bc7c9c
Tanner York <tannerwyork@gmail.com>
2019-08-30 22:17:37 -0700

    Revert Uncheck Item Functionality

--- checklist.py
+++ checklist.py
@@ -1,4 +1,5 @@
 from termcolor import colored
+from os import system, name
 
 checklist = list()
 
@@ -25,53 +26,47 @@ def list_all_items():
         index += 1
 
 def mark_completed(index):
-    item_index = valid_index(user_input('Index Number: '))
-    if item_index is 0 or item_index:
-        checklist[item_index] = colored(checklist[item_index], 'green')
-
-def mark_uncomplete(index):
-    item_index = valid_index(user_input('Index Number: '))
-    if item_index is 0 or item_index:
-        checklist[item_index] = colored(checklist[item_index], 'grey')
+    checklist[index] = colored(checklist[index], 'green')
 
 def select(function_code):
     if function_code == 'C' or function_code == 'c':
         input_item = user_input('Input item: ')
+        clear_terminal()
         create(input_item)
     
     elif function_code == 'R' or function_code == 'r':
         item_index = valid_index(user_input('Index Number: '))
+        clear_terminal()
         if item_index is 0 or item_index:
             print(read(item_index))
 
     elif function_code == 'U' or function_code == 'u':
-        item_index = valid_index(user_input('Index Number: '))
+        item_index = valid_index(user_input('Update Checklist Index Number: '))
         if item_index is 0 or item_index:
             update(item_index, user_input('New Item: '))
     
-    elif function_code == 'MD' or function_code == 'md':
-        item_index = valid_index(user_input('Index Number: '))
+    elif function_code == 'M' or function_code == 'm':
+        item_index = valid_index(user_input('Check Index Number: '))
+        clear_terminal()
         if item_index is 0 or item_index:
             mark_completed(item_index)
 
-    elif function_code == 'MU' or function_code == 'mu':
-        item_index = valid_index(user_input('Index Number: '))
-        if item_index is 0 or item_index:
-            mark_uncompleted(item_index)
-
     elif function_code == 'D' or function_code == 'd':
-        item_index = valid_index(user_input('Index Number: '))
+        item_index = valid_index(user_input('Delete Checklist Index Number: '))
+        clear_terminal()
         if item_index is 0 or item_index:
             delete(item_index)
 
     elif function_code == 'P' or function_code == 'p':
+        clear_terminal()
         list_all_items()
 
     elif function_code == 'Q' or function_code == 'q':
         return False
 
     else:
-        print('Unkown Option')
+        input('Unkown Option')
+        clear_terminal()
     return True
 
 def user_input(prompt):
@@ -79,15 +74,19 @@ def user_input(prompt):
     return user_input
 
 def valid_index(index):
-    if index.isnumeric() and int(index) <= len(checklist):
+    if index.isnumeric() and len(checklist) != 0 and int(index) <= len(checklist):
         return int(index)
     else:
-        print('Invaled, index is either not an int or out of range')
+        input('Invaled, index is either not an int or out of range')
         return False
 
+def clear_terminal():
+    system('clear')
+
 running = True
+clear_terminal()
 while running:
-    selection = user_input('Press C to add list, R to read from list, U to update item in list, MC to mark item as complete, MU to mark uncomplete, D to delete item in list, P to display all items, and Q to quit ')
+    selection = user_input('Press C to add list, R to read from list, U to update item in list, M to mark item as complete, D to delete item in list, P to display all items, and Q to quit ')
     running = select(selection)
 
 
