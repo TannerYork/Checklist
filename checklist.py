21865f896d37f502429efa1859db024d41ed721c
Tanner York <tannerwyork@gmail.com>
2019-08-30 21:34:47 -0700

    Add Uncheck Checklist Item

--- checklist.py
+++ checklist.py
@@ -25,7 +25,14 @@ def list_all_items():
         index += 1
 
 def mark_completed(index):
-    checklist[index] = colored(checklist[index], 'green')
+    item_index = valid_index(user_input('Index Number: '))
+    if item_index is 0 or item_index:
+        checklist[item_index] = colored(checklist[item_index], 'green')
+
+def mark_uncomplete(index):
+    item_index = valid_index(user_input('Index Number: '))
+    if item_index is 0 or item_index:
+        checklist[item_index] = colored(checklist[item_index], 'grey')
 
 def select(function_code):
     if function_code == 'C' or function_code == 'c':
@@ -42,6 +49,16 @@ def select(function_code):
         if item_index is 0 or item_index:
             update(item_index, user_input('New Item: '))
     
+    elif function_code == 'MD' or function_code == 'md':
+        item_index = valid_index(user_input('Index Number: '))
+        if item_index is 0 or item_index:
+            mark_completed(item_index)
+
+    elif function_code == 'MU' or function_code == 'mu':
+        item_index = valid_index(user_input('Index Number: '))
+        if item_index is 0 or item_index:
+            mark_uncompleted(item_index)
+
     elif function_code == 'D' or function_code == 'd':
         item_index = valid_index(user_input('Index Number: '))
         if item_index is 0 or item_index:
@@ -70,8 +87,7 @@ def valid_index(index):
 
 running = True
 while running:
-    selection = user_input(
-        'Press C to add list, R to read from list, U to update item in list, D to delete item in list, P to display all items, and Q to quit ')
+    selection = user_input('Press C to add list, R to read from list, U to update item in list, MC to mark item as complete, MU to mark uncomplete, D to delete item in list, P to display all items, and Q to quit ')
     running = select(selection)
 
 
